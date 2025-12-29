6e94744a6433a5d605e2bbb4fd1af3395fe24da0
egbuniwecemeka <egbuniwecemeka.com>
2025-12-29 01:53:24 +0100

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" A python script that calculates the simple interest of principal amount, ROI and time"""
+
+# Simple interest (I = P * R * T)
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+
+print(f'The simple interest is: {interest}')
+

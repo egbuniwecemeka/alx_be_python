30eb45ff804a839776252d18b85940a6e522b41b
egbuniwecemeka <egbuniwecemeka.com>
2026-01-07 04:30:49 +0100

    Calculate the Area of a Rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" A python script that calculates the area of a rectangle """
+
+length = 10
+width = 5
+
+# Area =  length * breadth
+area = length * width
+print(f'The area of the rectangle is: {area}')
+

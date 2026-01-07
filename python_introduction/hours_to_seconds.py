757f5e2bd45813075cb8cc81ac669a51c153bc8a
egbuniwecemeka <egbuniwecemeka.com>
2026-01-07 04:36:35 +0100

    Convert Hours to Seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A python script that converts hours to seconds """
+
+hours = 2
+
+# Seconds in 2 hours
+seconds = 2 * 60 * 60
+
+print(f'{hours} hour(s) is {seconds} seconds')
+

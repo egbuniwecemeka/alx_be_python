4edc798f455ef5f26aaf3bf0e5d9d129eae2e40c
egbuniwecemeka <egbuniwecemeka@gmail.com>
2026-04-05 21:16:32 +0100

    A simple multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""A simple multiplication"""
+"""A simple multiplication table"""
 
 # Iterates over factors of the outer loop
 for i in range (1, 9):
@@ -8,5 +8,5 @@ for i in range (1, 9):
     for j in range(1, 9):
         # Multiple of both factors
         product = i * j
-        print(f'{i} * {j} = {product}')
+        print(f'{i} * {j} = {product}', end='\t')
     print()

717c5ba6340bbb9ae42b6dc47495991a381a5832
egbuniwecemeka <egbuniwecemeka@gmail.com>
2026-04-11 06:25:15 +0100

    weather forcast

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,6 +5,7 @@
 # User's input on weather condition
 weather = str(input("What's the weather like today? (sunny/rainy/cold): "))
 
+
 if weather == 'sunny':
     print('Wear a t-shirt and sunglasses')
 elif weather == 'rainy':

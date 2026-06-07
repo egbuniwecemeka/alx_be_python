ab5b28fbd13a2a73c6cba7fe3cc31e00dd1133a2
egbuniwecemeka <egbuniwecemeka@gmail.com>
2026-06-07 13:27:10 +0100

    calculator for basic operations

---
+++ fns_and_dsa/practice-exercises/calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Performs basic arithmetic operations"""
+
+# Addition
+def add(a, b):
+    """Returns the sum of numbers"""
+    return a + b
+
+# Subtraction
+def subtract(a, b):
+    """Returns the difference between two numbers"""
+    return a - b
+
+# Multiplication
+def multiply(a, b):
+    """Returns the product of two numbers"""
+    return a * b
+
+# Division
+def divide(a, b):
+    """Returns the remainder f two numbers"""
+    return a / b
